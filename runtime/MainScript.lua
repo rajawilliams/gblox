65ea16bb642f47bd256ecf5a07ceb3f658a9b31d
lego man <rajawilliams24@gmail.com>
2020-06-20 15:44:00 -0700

    Sandboxing framework
    Trying to make it as modular as I can.

--- runtime/MainScript.lua
+++ runtime/MainScript.lua
@@ -1,6 +1,13 @@
+local Sandbox = require(script:WaitForChild("SBoxer"))
+
 local function RequireFunctionalModule(Name)
 	require(script:WaitForChild(Name))
 end
 
 RequireFunctionalModule("Tags")
 RequireFunctionalModule("Realism")
+
+Sandbox.RunSandbox([[
+	local test = "test"
+	print(test)
+]])
--- runtime/Sandboxer.lua
+++ runtime/Sandboxer.lua
@@ -1,1 +1,39 @@
 local loadstr = require(script:WaitForChild("Loadstring"))
+
+local APIs = script:WaitForChild("APIs")
+
+local DefaultLibraries = {
+	"bit32", "collectgarbage", "assert", "error",
+	"coroutine", "getmetatable", "ipairs", "newproxy",
+	"debug", "next", "pairs", "pcall", "print",
+	"math", "select", "setmetatable", "tonumber",
+	"os", "tostring", "type", "unpack", "xpcall",
+	"string", "_VERSION", "table", "delay", "tick",
+	"elapsedTime", "printidentity", "spawn",  "time",
+	"typeof", "version", "wait", "warn", "Enum"
+}
+
+local E = getfenv()
+
+local Sandbox = {}
+Sandbox.Env = {}
+
+function Sandbox.LoadIntoEnv(Key, Value)
+	if type(Value) == "table" then
+		for k, v in pairs(Value) do
+			Sandbox.LoadIntoEnv(k, v)
+		end
+		return
+	end
+	Sandbox.Env[Key] = Value
+end
+
+function Sandbox.RunSandbox(Str)
+	return loadstr(Str, Sandbox.Env)
+end
+
+for _, v in pairs(DefaultLibraries) do
+	Sandbox.LoadIntoEnv(v, E[v])
+end
+
+return Sandbox
